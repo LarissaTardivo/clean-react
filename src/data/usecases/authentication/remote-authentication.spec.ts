8a1b026b6824c1b6542472140eac68337ae6c8df
Larissa Tardivo <larissatardivo81@gmail.com>
2022-01-27 08:54:07 -0300

    test: ensure RemoteAuthentication calss HttpPostClient with correct URL

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,17 +1,5 @@
-interface HttpPostClient {
-  post (url: string): Promise<void>
-}
-
-class RemoteAuthentication {
-  constructor (
-    private readonly url: string,
-    private readonly httpPostClient: HttpPostClient
-  ) {}
-
-  async auth (): Promise<void> {
-    await this.httpPostClient.post(this.url)
-  }
-}
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
+import { RemoteAuthentication } from './remote-authentication'
 
 describe('RemoteAuthentication', () => {
   test('Should Call HttpClient with correct URL', async () => {
